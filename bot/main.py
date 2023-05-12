b65f50d52f3b0141cad8fad01681875e2dda0a87
alik <copier2@yandex.ru>
2023-05-12 21:17:00 +0500

    создание бота

---
+++ bot/main.py
@@ -0,0 +1,18 @@
+import telebot
+from googletrans import Translator
+from langdetect import detect
+
+translator = Translator()
+
+bot = telebot.TeleBot('5918043435:AAG7DChWzCt3yIHKgYk3Di16OlOphAvtNQU')
+
+
+@bot.message_handler(func=lambda m: True)
+def translate_massage(message):
+    src = detect(message.text)
+    dest = 'ru'
+    translated_text = translator.translate(message.text, src=src, dest=dest).text
+    bot.send_message(message.chat.id, translated_text)
+
+
+bot.polling(none_stop=True)
